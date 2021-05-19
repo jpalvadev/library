c7244e5966aae1598f1779709006d5f1fe9b8d9e
Juan Pablo Alvarez <jpalvadev@gmail.com>
2021-05-19 18:48:34 -0300

    Done!

---
+++ README.md
@@ -0,0 +1,26 @@
+# My Library
+
+This Project is part of The Odin Project's Curriculum
+
+- [Live Here](https://jpalvadev.github.io/Calculator/)
+
+## Things I learned - getting better at
+
+- Local Storage.
+
+- Handling problems with element position on mobile, specially problems that arise with fixed elements and the search bar.
+
+- Classes. This project is separated in 3 classes, UI, Data, and the App main class.
+  Hoping one day I will get back to this project and apply an MVC approach.
+
+- Async / Await.
+
+- Working with APIs
+
+## Description and considerations
+
+- A simple bookshelf.
+- You can search by author, title, ISBN, etc. Search is done thanks to the Google Books API.
+- Once a book is found, I try to find a better cover image using the OpenLibrary API, if it's not found, it shows de one with low res from Google.
+- It uses Local Storage, so data is not loss when the page reloads.
+- It's responsive. It looks good on mobile, well, at least on my phone :)
--- js/script.js
+++ js/script.js
@@ -9,10 +9,6 @@ const nav = document.querySelector('.nav');
 
 const overlay = document.querySelector('.overlay');
 
-let appHeight = '';
-// document.body.style.minHeight = window.innerHeight;
-// document.body.style.height = window.innerHeight;
-
 // Buttons
 const addBtn = document.querySelector('.add-book-btn--add');
 const okBtn = document.querySelector('.add-book-btn--ok');
@@ -28,7 +24,6 @@ const formPages = document.getElementById('pages');
 const formPagesRead = document.getElementById('pages-read');
 const checkboxRead = document.querySelector('.read-checkbox');
 
-// const header = document.querySelector('.form__heading');
 const searchSpinner = document.querySelector('.search-spinner');
 const formSpinner = document.querySelector('.form-spinner');
 
@@ -51,6 +46,7 @@ const inputFields = [
 const booksContainer = document.querySelector('.books-container');
 
 // BORRAR!!!!!!!!!!!
+const API_KEY = 'AIzaSyBBYV8OpUyNrm76ZUVGfgITYeOcZP00dsE';
 
 class Book {
   constructor(
@@ -72,9 +68,6 @@ class Book {
     this.isbn = isbn;
     this.complete = complete;
     this.pagesReadPerc = Math.floor((this.pagesRead * 100) / this.pages);
-    // this.pagesReadPerc = this.complete
-    //   ? 100
-    //   : Math.floor((this.pagesRead * 100) / this.pages);
   }
 }
 
@@ -107,13 +100,8 @@ class Data {
 
   updateReadStatusOnLS(isbn) {
     const books = this.getBooksFromLS();
-
     const index = books.findIndex((book) => book.isbn === isbn);
-    // console.log(books[index]);
-    // console.log(books[index].complete);
-
     books[index].complete = !books[index].complete;
-    // console.log(books[index].complete);
     localStorage.setItem('books', JSON.stringify(books));
 
     return books[index];
@@ -165,7 +153,6 @@ class UI {
   }
 
   showBookList(bookList) {
-    // console.log(bookList.items[0]);
     let divsContainer = document.createDocumentFragment();
     const imgNotFound =
       'http://books.google.com/books/content?id=CGL7DwAAQAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
@@ -202,27 +189,16 @@ class UI {
     formPages.value = book?.pageCount || '';
     formPagesRead.value = '';
 
-    // const img = await app.data.getBetterImg(book);
-    // console.log(img);
     app.UI.showElements(formSpinner);
     formCover.src = await app.data.getBetterImg(book);
     formCover.addEventListener('load', () => app.UI.hideElements(formSpinner), {
       once: true,
     });
     book.cover = formCover.src;
-
-    console.log('UI data', book);
-
     this.currentBook = book;
-    // app.UI.hideElements(formSpinner);
-    // console.log(img);
   }
 
   addBook(book) {
-    // const pagesReadPerc = book.complete
-    //   ? 100
-    //   : Math.floor((book.pagesRead * 100) / book.pages);
-
     booksContainer.innerHTML += `
     <div class="book">
       <div class="book__cover">
@@ -247,7 +223,9 @@ class UI {
           }%</p>
         </div>
         <div class="book__btns">
-          <button class="book__delete" data-isbn="${book.isbn}">Remove</button>
+          <button class="book__delete btn-style" data-isbn="${
+            book.isbn
+          }">Remove</button>
           <label class="toggle">
           <p>Read?</p>
             <input class="toggle__input" name="" type="checkbox" ${
@@ -277,11 +255,6 @@ class UI {
       );
 
     const checkbox = target.parentElement.querySelector('input');
-    console.log(checkbox.checked);
-
-    console.log(book);
-
-    console.log(progressBar);
 
     if (book.complete) {
       progressBar.style.width = '100%';
@@ -335,87 +308,98 @@ class App {
     this.UI.displayBooks();
   }
 
-  // Get books from API based on input text field
   async getSearchResults(keyword) {
-    // Show Spinner
     this.UI.showElements(searchSpinner);
 
-    // Hide Search box
-
-    // Get books from Google
     const bookList = await this.data.getBookList(keyword);
-    console.log(bookList);
-
-    // hide Spinner
     this.UI.hideElements(searchSpinner);
-
-    // Show books in the UI
     this.UI.showBookList(bookList);
-
     this.UI.showElements(searchResultsContainer);
     this.UI.hideElements(searchBooksForm);
     document.body.classList.remove('noscroll');
   }
 
   getBookData(isbn) {
-    // console.log(this.data.searchResults);
-    // console.log(isbn);
-
     const book = this.data.searchResults.items.filter((book) => {
       return book.volumeInfo?.industryIdentifiers?.[0].identifier === isbn;
     });
 
-    console.log(book);
-
     this.UI.showElements(formContainer, okBtn);
     this.UI.hideElements(searchResultsContainer);
-
     this.UI.showSelectedBook(book[0].volumeInfo);
-
     this.UI.clearSearchResults();
-
-    // document.body.classList.remove('noscroll');
     this.UI.scrollToTop();
-    // document.body.classList.add('noscroll');
     const formHeight = formContainer.offsetHeight;
-    console.log(formHeight);
     formContainer.style.height = `${formHeight}px`;
     document.body.style.minHeight = `${formHeight}px`;
     document.body.classList.add('noscroll');
+  }
 
-    // setTimeout(() => {
-    //   let rect = formContainer.getBoundingClientRect();
-    //   console.log(rect.top, rect.right, rect.bottom, rect.left);
-    //   addBtn.style.position = 'absolute';
-    //   addBtn.style.top = `${rect.bottom}px`;
-    // }, transitionDuration);
+  addBook() {
+    const title = formTitle.value;
+    const author = formAuthor.value;
+    const description =
+      app.UI.currentBook?.description?.slice(0, 200) || 'No description';
+    const pages = Number(formPages.value);
+    const pagesRead = Number(formPagesRead.value);
+    const cover = app.UI.currentBook.cover;
+    const isbn = app.UI.currentBook.industryIdentifiers[0].identifier;
+    const complete = checkboxRead.checked;
+    const book = new Book(
+      title,
+      author,
+      description,
+      pages,
+      pagesRead,
+      cover,
+      isbn,
+      complete
+    );
+
+    const validData = [formTitle, formAuthor, formPages].every((data) => {
+      if (!data.value) {
+        data.focus();
+        const missingData = data.previousElementSibling.textContent;
+        app.UI.showAlert(`Missing ${missingData} data`, false);
+        return false;
+      }
+      return true;
+    });
+
+    if (!validData) return;
+
+    if (book.pagesRead > book.pages) {
+      app.UI.showAlert("Pages read can't be higher than total pages", false);
+      return false;
+    }
+
+    document.body.classList.remove('noscroll');
+    app.UI.addBook(book);
+    app.data.addBook(book);
+    app.UI.showAlert('Book ADDED', true);
+    app.UI.hideElements(formContainer, okBtn, overlay);
+    addBtn.classList.remove('transform-to-cancel');
   }
 
   updateReadStatus(target, isbn) {
-    // Update Read status on LS
     const book = app.data.updateReadStatusOnLS(isbn);
-
-    // Update Read status on UI
     app.UI.updateReadStatus(target, book);
   }
 }
 
 const app = new App(new Data(), new UI());
-
 document.addEventListener('DOMContentLoaded', app.UI.displayBooks);
 
-// Add event listener for PLUS BUTTON
 ///////////////////
+// Add event listener for PLUS BUTTON
 addBtn.addEventListener('click', function (e) {
   form.reset();
-
-  searchInput.value = ''; // borrar search input
+  searchInput.value = '';
 
   formCover.src = './images/white.jpg'; // to avoid decentering of loading spinner
   e.preventDefault();
   if (!addBtn.classList.contains('transform-to-cancel')) {
     app.UI.showElements(searchBooksForm, booksContainer, overlay);
-    // searchBooksForm.scrollIntoView({ block: 'end', behavior: 'smooth' });
     app.UI.scrollToTop();
 
     document.body.classList.add('noscroll');
@@ -438,9 +422,6 @@ addBtn.addEventListener('click', function (e) {
 // Search button click event listener
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
-
-  appHeight = `${window.innerHeight}px`;
-
   if (!searchInput.value) {
     app.UI.showAlert('You have to type something...', false);
     return;
@@ -452,13 +433,7 @@ searchBtn.addEventListener('click', (e) => {
 
 // Add event listener for targeting individual book using event delegation
 searchResultsContainer.addEventListener('click', (e) => {
-  // console.log(e.target);
   if (!e.target.closest('div').classList.contains('result')) return;
-  // console.log("si");
-
-  // app.getBook();
-  console.log(app.data.searchResults);
-  // const bookId = e.target.closest("div").id;
   const targetISBN = e.target
     .closest('div')
     .querySelector('.result__isbn')
@@ -467,66 +442,18 @@ searchResultsContainer.addEventListener('click', (e) => {
   app.getBookData(targetISBN);
 });
 
-okBtn.addEventListener('click', () => {
-  // Create a book object
-  const title = formTitle.value;
-  const author = formAuthor.value;
-  const description =
-    app.UI.currentBook?.description?.slice(0, 200) || 'No description';
-  const pages = Number(formPages.value);
-  const pagesRead = Number(formPagesRead.value);
-  const cover = app.UI.currentBook.cover;
-  const isbn = app.UI.currentBook.industryIdentifiers[0].identifier;
-  const complete = checkboxRead.checked;
-  const book = new Book(
-    title,
-    author,
-    description,
-    pages,
-    pagesRead,
-    cover,
-    isbn,
-    complete
-  );
-
-  if (pagesRead > pages) {
-    // console.log(Number(pagesRead), pages);
-
-    app.UI.showAlert(
-      'You cannot read more pages than there are in the book... dah!',
-      false
-    );
-    return;
-  }
-
-  document.body.classList.remove('noscroll');
-
-  app.UI.addBook(book);
-  app.data.addBook(book);
-
-  app.UI.hideElements(formContainer, okBtn, overlay);
-
-  addBtn.classList.remove('transform-to-cancel');
-});
+okBtn.addEventListener('click', app.addBook);
 
 // Using mouseup to prevent the event from firing twice
 booksContainer.addEventListener('mouseup', (e) => {
-  // console.log(e.target);
-
   if (e.target.closest('label')?.classList.contains('toggle')) {
-    // e.stopPropagation();
     const isbn = e.target.parentElement.previousElementSibling.dataset.isbn;
 
     app.updateReadStatus(e.target, isbn);
   }
-  // e.preventDefault();
-
   if (e.target.classList.contains('book__delete')) {
-    // Remove book from UI
-
     app.UI.deleteBook(e.target);
-
-    // Remove book from LS
     app.data.removeBookFromLS(e.target.dataset.isbn);
+    app.UI.showAlert('Book DELETED', true);
   }
 });
